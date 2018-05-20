ee9538155fea3b99396917d69394562a743c5f77
min <0s.dongmin@gmail.com>
2018-05-21 05:13:52 +0900

    Message UI component

---
+++ src/component/ui/Message/Message.js
@@ -0,0 +1,38 @@
+// import external dependencies
+import classnames from 'classnames';
+// import internal dependencies
+import Component from 'service/component/Component';
+
+// import assets
+import './Message.scss';
+
+// return UserList class
+
+export default class Message extends Component {
+  /**
+   *
+   * @param {string} props.text innerHtml
+   * @param {string} props.class
+   */
+
+  update(nextProps) {
+    if (this.props.text !== nextProps.text) {
+      this.element.innerHTML = nextProps.text;
+    }
+  }
+
+  render(link) {
+    const html = (
+      `<div
+        class="${classnames(
+        'Message',
+        {
+          [this.props.class]: this.props.class,
+        })}"
+      >
+        ${this.props.text}
+      </div>`
+    );
+    super.render(link, html);
+  }
+}
